9e8c871f67101a6580d38ab097538832dfd70bd9
nearworld <artemismars2@outlook.com>
2024-10-22 16:25:33 +0900

    feat: 메인페이지 디자인

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='flex-col justify-center items-center'>
-      <h1 className='text-2xl'>사회적 협동조합</h1>
-      <h1>청소년 자립학교</h1>
-      <span>직원 전용 서비스</span>
-      <Image src='/logo.png' width={300} height={300} alt='이미지 로고' />
-      <button></button>
+    <div className='flex flex-col justify-center items-center p-3 min-w-[340px] max-w-[500px] m-auto bg-white h-full'>
+      <Title />
+      <div className='flex justify-center w-full'>
+        <Image src='/logo.png' width={300} height={300} alt='이미지 로고' />
+      </div>
+      <Button className='w-full bg-yellow-500 hover:bg-yellow-600 text-xl p-6'>
+        카카오 로그인
+      </Button>
     </div>
+  );
+}
+
+function Title() {
+  return (
+    <>
+      <h1 className='text-7xl text-center'>사회적 협동조합</h1>
+      <h1 className='text-7xl text-center'>청소년 자립학교</h1>
+      <p className='w-full'>
+        <span className='float-end'>직원 전용 서비스</span>
+      </p>
+    </>
   );
 }
