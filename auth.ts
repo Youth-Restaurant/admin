548070015a0583056b4731ad42709c64b69569e9
nearworld <artemismars2@outlook.com>
2024-10-24 22:07:31 +0900

    feat: authjs의 카카오 로그인 세션 로직 추가

--- auth.ts
+++ auth.ts
@@ -1,12 +1,56 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
 import Kakao from 'next-auth/providers/kakao';
+
+type KakaoAccount = {
+  profile_nickname_needs_agreement: boolean;
+  profile_image_needs_agreement: boolean;
+  profile: {
+    nickname: string;
+    thumbnail_image_url: string;
+    profile_image_url: string;
+    is_default_image: boolean;
+    is_default_nickname: boolean;
+  };
+};
+
+declare module 'next-auth' {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      nickname: string;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession['user'];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Kakao({
+      clientId: process.env.AUTH_KAKAO_ID,
+      clientSecret: process.env.AUTH_KAKAO_SECRET,
       async profile(profile) {
         return { ...profile };
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      if (token.kakao_account) {
+        const account = token.kakao_account as KakaoAccount;
+        session.user.nickname = account.profile.nickname;
+        session.user.image = account.profile.profile_image_url;
+      }
+      return session;
+    },
+  },
 });
