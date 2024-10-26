8670af65a5ee9b9ba862cd81305bfe3b28b67e47
nearworld <artemismars2@outlook.com>
2024-10-26 10:14:23 +0900

    fix: prevent overflow of the Post List on Notices page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { mockPosts } from '@/mocks/posts';
 // 공지사항 페이지
 export default async function Home() {
   return (
-    <main className='p-[10px]'>
+    <main className='p-[10px] h-[calc(100vh-120px)] overflow-y-auto'>
       <ul className='flex flex-col gap-3'>
         {mockPosts.map((post) => (
           <li key={post.id}>
