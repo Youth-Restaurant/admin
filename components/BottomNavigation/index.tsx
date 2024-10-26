f62e3b1b7ae8cfdb45ea0367e5805cae6c359f68
nearworld <artemismars2@outlook.com>
2024-10-26 10:06:59 +0900

    fix: BottomNavigation z-index to 50 and bg-white

--- components/BottomNavigation/index.tsx
+++ components/BottomNavigation/index.tsx
@@ -3,7 +3,7 @@ import ProfileImage from './ProfileImage';
 
 export default function BottomNavigation() {
   return (
-    <nav className='absolute right-0 left-0 bottom-0 w-full border-t-[0.3px]'>
+    <nav className='absolute right-0 left-0 bottom-0 w-full border-t-[0.3px] z-50 bg-white'>
       <ul className='flex w-full justify-around py-3'>
         <Navigation />
         <ProfileImage />
