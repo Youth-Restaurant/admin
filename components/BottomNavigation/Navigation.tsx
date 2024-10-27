42593cfc2b3f5e187bf7e21a9943568f3a4fe330
nearworld <artemismars2@outlook.com>
2024-10-27 18:48:19 +0900

    fix: fix the build error in Navigation.tsx

--- components/BottomNavigation/Navigation.tsx
+++ components/BottomNavigation/Navigation.tsx
@@ -2,11 +2,9 @@
 import { BellIcon, BoxIcon, Calendar, File } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 
 export default function Navigation() {
   const pathname = usePathname();
-  const [selectedIndex, setSelectedIndex] = useState(0); // 선택된 아이템의 인덱스를 추적
   const navItems = [
     { icon: BellIcon, label: '공지사항', href: '/' },
     { icon: BoxIcon, label: '재고관리', href: '/inventory' },
@@ -23,10 +21,7 @@ export default function Navigation() {
         return (
           <li key={index} className='flex flex-col items-center'>
             <Link href={item.href}>
-              <div
-                className='flex flex-col items-center cursor-pointer'
-                onClick={() => setSelectedIndex(index)}
-              >
+              <div className='flex flex-col items-center cursor-pointer'>
                 <IconComponent strokeWidth={isActive ? 2 : 1} />
                 <span className={isActive ? 'font-semibold' : ''}>
                   {item.label}
